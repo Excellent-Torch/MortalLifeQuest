53bc6be994bfae84f1878ce6a2c05a55ec6a300d
Excellent_Torch <ishanmaduranga42@gmail.com>
2025-04-18 22:24:20 +0100

    Initial Commit

---
+++ Source/MortalLifeQuest/MortalLifeQuestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MortalLifeQuestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMortalLifeQuestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMortalLifeQuestGameMode();
+};
+
+
+
